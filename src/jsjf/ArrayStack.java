c191a5b46aec614e3c0830ce456d11da8019401d
rkaune <rkaune@7HPL6Y1.Student.UGDSB.ED>
2019-06-27 15:18:16 -0400

    Push to rkaune REPO commit ArrayStackTester

--- src/jsjf/ArrayStack.java
+++ src/jsjf/ArrayStack.java
@@ -6,121 +6,136 @@ import java.util.Arrays;
 /**
  * An array implementation of a stack in which the bottom of the
  * stack is fixed at index 0.
- * 
+ *
  * @author Java Foundations
  * @version 4.0
+ * NOTE: This version contains the implementation of several
+ * methods left as a programming exercise.
  */
 public class ArrayStack<T> implements StackADT<T>
 {
-	private final static int DEFAULT_CAPACITY = 100;
+    private final static int DEFAULT_CAPACITY = 100;
 
-	private int top;  
-	private T[] stack;
+    private int top;
+    private T[] stack;
 
-	/**
-	 * Creates an empty stack using the default capacity.
-	 */
-	public ArrayStack()
-	{
-		this(DEFAULT_CAPACITY);
-	}
+    /**
+     * Creates an empty stack using the default capacity.
+     */
+    public ArrayStack()
+    {
+        // default contstructor should also have top = -1
+        this(DEFAULT_CAPACITY);
+        top =-1;
+    }
 
-	/**
-	 * Creates an empty stack using the specified capacity.
-	 * @param initialCapacity the initial size of the array 
-	 */
-	public ArrayStack(int initialCapacity)
-	{
-		top = 0;
-		stack = (T[])(new Object[initialCapacity]);
-	}
+    /**
+     * Creates an empty stack using the specified capacity.
+     * @param initialCapacity the initial size of the array
+     */
+    public ArrayStack(int initialCapacity)
+    {
+        // here is where I modified the value of top from top=0 to top=-1
+        top = -1;
+        stack = (T[])(new Object[initialCapacity]);
+    }
 
-	/**
-	 * Adds the specified element to the top of this stack, expanding
-	 * the capacity of the array if necessary.
-	 * @param element generic element to be pushed onto stack
-	 */
-	public void push(T element)
-	{
-		if (size() == stack.length) 
-			expandCapacity();
+    /**
+     * Adds the specified element to the top of this stack, expanding
+     * the capacity of the array if necessary.
+     * @param element generic element to be pushed onto stack
+     */
+    public void push(T element)
+    {
+        if (size() == stack.length)
+            expandCapacity();
+        
+        // increment the top variable, then assign the element--> stack[top], top then points to actual top of the stack
+        top++;
+        stack[top] = element;
+        
+    }
 
-		stack[top] = element;
-		top++;
-	}
+    /**
+     * Creates a new array to store the contents of this stack with
+     * twice the capacity of the old one.
+     */
+    private void expandCapacity()
+    {
+        
+        stack = Arrays.copyOf(stack, stack.length * 2);
+        // trace on expandCapacity()
+        System.out.println("Stack length is now " + stack.length);
+    }
 
-	/**
-	 * Creates a new array to store the contents of this stack with
-	 * twice the capacity of the old one.
-	 */
-	private void expandCapacity()
-	{
-		stack = Arrays.copyOf(stack, stack.length * 2);   
-	}
+    /**
+     * Removes the element at the top of this stack and returns a
+     * reference to it.
+     * @return element removed from top of stack
+     * @throws EmptyCollectionException if stack is empty
+     */
+    public T pop() throws EmptyCollectionException
+    {
+        if (isEmpty())
+            throw new EmptyCollectionException("stack");
 
-	/**
-	 * Removes the element at the top of this stack and returns a
-	 * reference to it. 
-	 * @return element removed from top of stack
-	 * @throws EmptyCollectionException if stack is empty 
-	 */
-	public T pop() throws EmptyCollectionException
-	{
-		if (isEmpty())
-			throw new EmptyCollectionException("stack");
+        
+        T result = stack[top];
+        stack[top] = null;
+        
+        // decrement top to below the element
+        top--;
+        
+        // result is now the current pointer to stack top
+        return result;
+    }
 
-		top--;
-		T result = stack[top];
-		stack[top] = null; 
+    /**
+     * Returns a reference to the element at the top of this stack.
+     * The element is not removed from the stack.
+     * @return element on top of stack
+     * @throws EmptyCollectionException if stack is empty
+     */
+    public T peek() throws EmptyCollectionException
+    {
+        if (isEmpty())
+            throw new EmptyCollectionException("stack");
+        
+        // element is the actual top 
+        return stack[top];
+    }
 
-		return result;
-	}
+    /**
+     * Returns true if this stack is empty and false otherwise.
+     * @return true if this stack is empty
+     */
+    public boolean isEmpty()
+    {
+        // modified isEmpty() 
+        return (top == -1);
+    }
 
-	/**
-	 * Returns a reference to the element at the top of this stack.
-	 * The element is not removed from the stack.  
-	 * @return element on top of stack
-	 * @throws EmptyCollectionException if stack is empty
-	 */
-	public T peek() throws EmptyCollectionException
-	{
-		if (isEmpty())
-			throw new EmptyCollectionException("stack");
+    /**
+     * Returns the number of elements in this stack.
+     * @return the number of elements in the stack
+     */
+    public int size()
+    {
+        return top+1;
+    }
 
-		return stack[top-1];
-	}
+    /**
+     * Returns a string representation of this stack.
+     * @return a string representation of the stack
+     */
+    public String toString()
+    {
+        String result = "";
 
-	/**
-	 * Returns true if this stack is empty and false otherwise. 
-	 * @return true if this stack is empty
-	 */
-	public boolean isEmpty()
-	{
-		// To be completed as a Programming Project
-		
-		return true;  // temp
-	}
+        for (int scan=0; scan < top; scan++)
+             result = result + stack[scan] + "\n";
 
-	/**
-	 * Returns the number of elements in this stack.
-	 * @return the number of elements in the stack
-	 */
-	public int size()
-	{
-		// To be completed as a Programming Project
-		
-		return 0;  // temp
-	}
-
-	/**
-	 * Returns a string representation of this stack. 
-	 * @return a string representation of the stack
-	 */
-	public String toString()
-	{
-		// To be completed as a Programming Project
-		
-		return "";  // temp
-	}
+        return result;
+    }
 }
 
--- src/jsjf/ArrayStackTester.java
+++ src/jsjf/ArrayStackTester.java
@@ -16,7 +16,26 @@ public class ArrayStackTester {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        //ArrayStack<Integer> arrayStack = new ArrayStack<Integer>();
+        ArrayStack<Integer> arrayStack = new ArrayStack<Integer>(1);
+        arrayStack.push(1);
+        System.out.println("Stack size: " + arrayStack.size());
+        arrayStack.push(2);
+        System.out.println("Stack size: " + arrayStack.size());
+        arrayStack.push(3);
+        
+//        arrayStack.push(3);
+        System.out.println("Stack size: " + arrayStack.size());
+        System.out.println("Stack before peek " + arrayStack);
+        System.out.println("Calling peek method ");
+        System.out.println(arrayStack.peek());
+        System.out.println("Stack before pop " + arrayStack);
+        System.out.println("Calling pop method ");
+        System.out.println(arrayStack.pop());
+        System.out.println("Stack after pop " + arrayStack);
+        System.out.println("Stack size: " + arrayStack.size());
+        System.out.println(arrayStack);
     }
     
 }
